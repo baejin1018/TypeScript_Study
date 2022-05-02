e025db1cb6d899709290334c83e476a5c8fc8c26
Bae-Jin-Young <bae20051018@gmail.com>
2022-05-02 23:23:31 +0900

    Study : Ts

---
+++ 애플코딩TS/접근지정자/index.js
@@ -0,0 +1,8 @@
+var User = /** @class */ (function () {
+    function User(a) {
+        this.name = "kim";
+        this.name = a;
+    }
+    return User;
+}());
+var User1 = new User("park");
---
+++ 애플코딩TS/접근지정자/index.ts
@@ -0,0 +1,8 @@
+class User {
+  public name = "kim";
+  constructor(a) {
+    this.name = a;
+  }
+}
+
+const User1 = new User("park");
